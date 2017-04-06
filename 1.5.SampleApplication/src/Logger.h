7ec15ab7f73dae3b2c791c5ec455fb55ab75f9a0
Michael Kapelko <kornerr@gmail.com>
2017-04-06 09:28:14 +0700

    Add Logger class to print OpenSceneGraph notifications to stdout

---
+++ 1.5.SampleApplication/src/Logger.h
@@ -0,0 +1,77 @@
+
+/*
+This file is part of OpenSceneGraph cross-platform guide:
+  https://github.com/OGStudio/openscenegraph-cross-platform-guide
+
+Copyright (C) 2017 Opensource Game Studio
+
+This software is provided 'as-is', without any express or implied
+warranty.  In no event will the authors be held liable for any damages
+arising from the use of this software.
+
+Permission is granted to anyone to use this software for any purpose,
+including commercial applications, and to alter it and redistribute it
+freely, subject to the following restrictions:
+
+1. The origin of this software must not be misrepresented; you must not
+   claim that you wrote the original software. If you use this software
+   in a product, an acknowledgment in the product documentation would be
+   appreciated but is not required.
+2. Altered source versions must be plainly marked as such, and must not be
+   misrepresented as being the original software.
+3. This notice may not be removed or altered from any source distribution.
+*/
+
+#ifndef OPENSCENEGRAPH_CROSS_PLATFORM_GUIDE_LOGGER_H
+#define OPENSCENEGRAPH_CROSS_PLATFORM_GUIDE_LOGGER_H
+
+// Notify header provides NotifyHandler, which we use to get OpenSceneGraph notifications.
+#include <osg/Notify>
+// iostream gives access to printf().
+#include <iostream>
+
+// This class prints OpenSceneGraph notifications to stdout.
+class Logger : public osg::NotifyHandler
+{
+    public:
+        Logger() { }
+
+        // Override NotifyHandler::notify() to receive OpenSceneGraph notifications.
+        void notify(osg::NotifySeverity severity, const char *message)
+        {
+            // Simply print each notification to stdout.
+            printf("OSG/%s %s", severityToStr(severity).c_str(), message);
+        }
+
+    private:
+        // Convert NotifySeverity enum value to string.
+        std::string severityToStr(osg::NotifySeverity severity)
+        {
+            switch (severity)
+            {
+                case osg::DEBUG_FP:
+                    // Verbose.
+                    return "V";
+                case osg::DEBUG_INFO:
+                    // Debug.
+                    return "D";
+                case osg::NOTICE:
+                case osg::INFO:
+                    // Info.
+                    return "I";
+                case osg::WARN:
+                    // Warning.
+                    return "W";
+                case osg::FATAL:
+                case osg::ALWAYS:
+                    // Error.
+                    return "E";
+                default:
+                    // Default to Debug.
+                    return "D";
+            }
+        }
+};
+
+#endif // OPENSCENEGRAPH_CROSS_PLATFORM_GUIDE_LOGGER_H
+
